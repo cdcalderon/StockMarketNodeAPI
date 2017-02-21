1948f31b8a4193ffa3467a565b882c02c3898786
carloscalderon <cdcalderon@gmail.com>
2017-02-21 01:22:34 -0600

    Add populate earnings service

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var { ObjectID } = require('mongodb'); 
+var earningJobs = require('./services/earnings.jobs');
 
-
+earningJobs.populateEarnings('2/17/2017', '1/1/2018');
 var { mongoose } = require('./db/mongoose');
 var { StockEarning } = require('./models/stockEarning');
 
