4c1a369fe765ec53e7e267954283aa2fcc1fa49d
carloscalderon <cdcalderon@gmail.com>
2017-02-25 13:54:39 -0600

    refactor and configure beautify

--- server/services/earnings.jobs.js
+++ server/services/earnings.jobs.js
@@ -1,77 +1,85 @@
 var schedule = require('node-schedule');
-const { ObjectID } = require('mongodb');
-const { StockEarning } = require('./../models/stockEarning');
+const {
+    ObjectID
+} = require('mongodb');
+const {
+    StockEarning
+} = require('./../models/stockEarning');
 const axios = require('axios');
 
 var dateString2 = '2017-03-15';
 var earningUrl = 'https://www.bloomberg.com/markets/api/calendar/earnings/US?locale=en&date=';
 
 var populateEarnings = (startDateString, endDateString) => {
-  
-
-  let i = 0;
+    let i = 0;
     let datesArray = getDatesArray(startDateString, endDateString);
 
-    schedule.scheduleJob('*/5 * * * *', function(){
-      getEarningInformation(datesArray[i++]).then((response) => {
-         let dateString = response.config.dateString;
-          if(response.status === 200) {
-            if(response.data.events == null){
-              throw new Error('Unable to find records');
-            }
+    schedule.scheduleJob('*/1 * * * *', function() {
+        getEarningInformation(datesArray[i++]).then((response) => {
+            let dateString = response.config.dateString;
+            if (response.status === 200) {
+                if (response.data.events == null) {
+                    throw new Error('Unable to find records');
+                }
 
-            for(event of response.data.events) {
-              let symbol = event.company.ticker.split(':')[0];
+                for (event of response.data.events) {
+                    let symbol = event.company.ticker.split(':')[0];
 
-              StockEarning.find({
-                symbol:symbol, 
-                reportDateStr: response.config.dateString
-              }).then((earnings) => {
-                if (!earnings.length) {
-                    console.log('Dont exits, so i can add it');
-                        var stockEarning = new StockEarning({
+                    StockEarning.find({
                         symbol: symbol,
                         reportDateStr: response.config.dateString
-                        });
-
-                      stockEarning.save().then((doc) => {
-                        console.log('success saving.. : ', doc);
-                      }, (e) => {
-                        console.log('error saving.. : ', e);
-                      }); 
-                } else {
-                  console.log('exits, so i cant add it');
+                    }).then((earnings) => {
+                        insertEarning(earnings, symbol, dateString);
+                    });
                 }
-              });
             }
-          }
-       }).catch((e) => {
-            if(e.code === '') {
-            console.log('Unable to connect to API servers.')
+        }).catch((e) => {
+            if (e.code === '') {
+                console.log('Unable to connect to API servers.')
             } else {
-            console.log(e.message);
+                console.log(e.message);
             }
-          })
+        })
     });
 }
 
 var getEarningInformation = (dateString) => {
-    return axios.get(`${earningUrl + dateString}`, {dateString});
+    return axios.get(`${earningUrl + dateString}`, {
+        dateString
+    });
 }
 
 var getDatesArray = (startDateString, endDateString) => {
-  var start = new Date(startDateString);
+    var start = new Date(startDateString);
     var end = new Date(endDateString);
-  let datesArray = [];
-  while(start < end){
-       var newDate = start.setDate(start.getDate() + 1);
-       start = new Date(newDate);
-       startDateString = `${start.getFullYear() + '-' + 
+    let datesArray = [];
+    while (start < end) {
+        var newDate = start.setDate(start.getDate() + 1);
+        start = new Date(newDate);
+        startDateString = `${start.getFullYear() + '-' + 
                            (start.getMonth() + 1) + '-' + 
                             start.getDate()}`;
-       datesArray.push(startDateString);
-  }
-  return datesArray;
+        datesArray.push(startDateString);
+    }
+    return datesArray;
+}
+
+var insertEarning = (earnings, symbol, dateString) => {
+    if (!earnings.length) {
+        console.log('Dont exits, so i can add it');
+        var stockEarning = new StockEarning({
+            symbol: symbol,
+            reportDateStr: dateString
+        });
+
+        stockEarning.save().then((doc) => {
+            console.log('success saving.. : ', doc);
+        }, (e) => {
+            console.log('error saving.. : ', e);
+        });
+    } else {
+        console.log('exits, so i can not add it');
+    }
 }
 
 module.exports.populateEarnings = populateEarnings;
